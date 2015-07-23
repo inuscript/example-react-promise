440821c9c414dd22a5c1079aa8beaf7db494092d
inuscript <inuscript@gmail.com>
2015-07-23 21:39:51 +0900

    without flux sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 
+var randomDelay = function(fn) {
+  setTimeout(() => {
+    fn()
+  }, Math.random() * 500)
+}
 class CounterStorage{
   constructor(){
     this.num = 1
   }
   getNum(){
     return new Promise((resolve, reject) =>{
-      setTimeout(() => {
+      randomDelay(() => {
         resolve(this.num)
-      }, 500)
+      })
     })
   }
   increment(){
     return new Promise((resolve, reject) => {
-      this.num++
-      resolve();
+      randomDelay(() => {
+        this.num++
+        resolve();
+      })
+    })
+  }
+  decrement(){
+    return new Promise((resolve, reject) => {
+      randomDelay(() => {
+        this.num--
+        resolve();
+      })
     })
   }
 }
@@ -27,28 +42,39 @@ class Counter extends React.Component{
 
 class Button extends React.Component{
   render(){
-    return <button>+</button>
+    return <button onClick={this.props.onClick}>{this.props.label}</button>
   }
 }
 
 export default class App extends React.Component{
   constructor(){
     super()
+    this.storage = new CounterStorage()
     this.state = {count: 0}
-    let storage = new CounterStorage()
-    let gen = (function *(){
-      yield storage.getNum()
-    })()
-    console.log(gen)
-    let num = gen().next()
-    this.setState({
-      count: num
+    this.syncStorage();
+  }
+  syncStorage(){
+    this.storage.getNum().then((num) => {
+      this.setState({
+        count: num
+      })
+    })
+  }
+  doIncrement(){
+    this.storage.increment().then((num) => {
+      this.syncStorage()
+    })
+  }
+  doDecrement(){
+    this.storage.decrement().then((num) => {
+      this.syncStorage()
     })
   }
   render(){
     return <div> 
       <Counter count={this.state.count} />
-      <Button />
+      <Button  onClick={this.doIncrement.bind(this)} label="+"/>
+      <Button  onClick={this.doDecrement.bind(this)} label="-"/>
     </div>
   }
 }
